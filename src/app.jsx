22b169ce571f8c5f0d650d2fd5a93b4c8071e5b8
olesiklesha <olesik736@gmail.com>
2024-05-14 19:23:13 +0300

    feat: create a base
    * add tailwindcss
    * add editor config
    * create a base files

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export const App = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+};
