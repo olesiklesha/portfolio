597b6a6a0bd0b71f76d85cf74ef55b3d733f2607
olesiklesha <olesik736@gmail.com>
2024-05-15 12:50:25 +0300

    feat: start implement island rotation

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import {Canvas} from "@react-three/fiber";
-import {Suspense} from "react";
+import {Suspense, useEffect, useState} from "react";
 import {Loader} from "../../components/index.js";
 import {Island, Sky} from "../../models/index.js";
 
@@ -11,6 +11,8 @@ import {Island, Sky} from "../../models/index.js";
 {/*</div>*/
 }
 export const Home = () => {
+    const [isRotating, setIsRotating] = useState(false);
+
     const adjustIslandForScreenSize = () => {
         let screenScale = [1, 1, 1];
         let screenPosition = [0, -6.5, -43];
@@ -25,10 +27,12 @@ export const Home = () => {
 
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
+
     return (
         <section className="w-full h-screen relative">
-            <Canvas className="w-full h-full bg-transparent"
-                    camera={{near: 0.1, far: 1000}}
+            <Canvas
+                className={`w-full h-full bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+                camera={{near: 0.1, far: 1000}}
             >
                 <Suspense fallback={<Loader/>}>
                     <directionalLight position={[10, 1, 1]} intensity={2}/>
@@ -40,6 +44,8 @@ export const Home = () => {
                         position={islandPosition}
                         scale={islandScale}
                         rotation={islandRotation}
+                        isRotating={isRotating}
+                        setIsRotating={setIsRotating}
                     />
                 </Suspense>
             </Canvas>
