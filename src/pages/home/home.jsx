f69987951b6001685d41fa3ebc4ee15c8f16523b
olesiklesha <olesik736@gmail.com>
2024-05-15 11:49:46 +0300

    feat: start creating home page
    * add assets
    * add packages
    * set up vite config
    * create loader component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,50 @@
+import {Canvas} from "@react-three/fiber";
+import {Suspense} from "react";
+import {Loader} from "../../components/index.js";
+import {Island} from "../../models/index.js";
+
+{/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">*/
+}
+{/*    popup*/
+}
+{/*</div>*/
+}
 export const Home = () => {
+    const adjustIslandForScreenSize = () => {
+        let screenScale = [1, 1, 1];
+        let screenPosition = [0, -0.65, -43];
+        let rotation = [0.1, 4.7, 0];
+
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+            screenPosition = [0, -0.65, -43];
+        }
+
+        return [screenScale, screenPosition, rotation];
+    };
+
+    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
     return (
-        <div>
-            home
-        </div>
+        <section className="w-full h-screen relative">
+            <Canvas className="w-full h-full bg-transparent"
+                    camera={{near: 0.1, far: 1000}}
+            >
+                <Suspense fallback={<Loader/>}>
+                    <ambientLight/>
+                    <directionalLight/>
+                    <pointLight/>
+                    <spotLight/>
+                    <hemisphereLight/>
+
+                    <Island
+                        position={islandPosition}
+                        scale={islandScale}
+                        rotation={islandRotation}
+                    />
+                </Suspense>
+            </Canvas>
+        </section>
     );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-})
+    plugins: [react()],
+    assetsInclude: ['**/*.glb'],
+    server: {
+        host: true,
+        port: 3000,
+    },
+});
