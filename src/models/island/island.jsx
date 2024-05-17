71d18b99c351d55a43365245cf787dd9d4efcac3
olesiklesha <olesik736@gmail.com>
2024-05-17 16:19:06 +0300

    refactor: rotation speed on key down

--- src/models/island/island.jsx
+++ src/models/island/island.jsx
@@ -57,13 +57,13 @@ export const Island = ({isRotating, setIsRotating, setCurrentStage, ...props}) =
         if (e.key === 'ArrowLeft') {
             if (!isRotating) setIsRotating(true);
             islandRef.current.rotation.y += 0.01 * Math.PI;
-            rotationSpeed.current = 0.007;
+            rotationSpeed.current = 0.0125;
         }
 
         if (e.key === 'ArrowRight') {
             if (!isRotating) setIsRotating(true);
             islandRef.current.rotation.y -= 0.01 * Math.PI;
-            rotationSpeed.current = -0.007;
+            rotationSpeed.current = -0.0125;
         }
     };
     const handleKeyUp = (e) => {
